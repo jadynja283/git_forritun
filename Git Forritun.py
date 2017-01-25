4f0b618a63cc6c31d7be42dfa012fc37a260786a
Jadyn Margrét Jackson <jadynja283@nemi.tskoli.is>
2017-01-25 10:48:08 +0000

    Git Forritun Dæmi 1

---
+++ Git Forritun.py
@@ -0,0 +1,8 @@
+#Git verkefni Jadyn Margrét Jackson
+print("1.")
+
+tala1 = int(input("Sláðu inn tölu 1 "))
+tala2 = int(input("Sláðu inn tölu 2 "))
+
+print("Hér eru tölurnar lagðar saman", tala1+tala2)
+print("Hér eru tölurnar margfaldaðar saman", tala1*tala2)
